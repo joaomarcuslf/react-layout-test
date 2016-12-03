c1bdb0de120c498fd59744c06d28e4cf0ac17289
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-12-03 19:57:48 -0200

    Added icons to LandingPage

--- app/Components/Container/LandingPage/LandingPage.jsx
+++ app/Components/Container/LandingPage/LandingPage.jsx
@@ -1,11 +1,21 @@
 /* @flow */
 import React from 'react';
 
+import Icon from '../../Icon/Icon.jsx';
+
 export default class LandingPage extends React.Component {
   propTypes: {}
 
   constructor() {
     super();
+
+    this.state = {
+      icons: [
+        { iconName: 'plane', alt: 'Plane Icon' },
+        { iconName: 'wallet', alt: 'Wallet Icon' },
+        { iconName: 'case', alt: 'Case Icon' }
+      ]
+    }
   }
 
   shouldComponentUpdate(): boolean {
@@ -13,18 +23,27 @@ export default class LandingPage extends React.Component {
   }
 
   render(): ?React$Element<div> {
+    let iconsElm = this.state.icons.map(
+      (elem: object, index: integer): ?React$Element<Icon> => {
+        return <Icon iconName={elem.iconName} alt={elem.alt} key={index} />;
+      });
+
     return(
       <div className='landing-page'>
         <div className='overlay-red bg-city'>
           <div className='container'>
             <h1 className='title is-fullwidth'>Nessas férias, escolha o produto certo para você.</h1>
             <h2 className='subtitle is-half is-offset-one-quarter'>Participe da nossa Mailing List.</h2>
-            <p className='control form is-half is-offset-one-quarter'>
+            <form className='control form is-half is-offset-one-quarter'>
               <input className='input form-content is-half' type='email' placeholder='Seu e-mail' />
               <a className='button form-content is-outlined is-half'>
                 Saiba Mais
               </a>
-            </p>
+            </form>
+
+            <div className='icons-container'>
+              {iconsElm}
+            </div>
           </div>
         </div>
       </div>
