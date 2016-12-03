e8ab4708be2ae092dc3b697cc9533595d782a6fc
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-12-03 17:57:52 -0200

    Added first landing-page content

--- app/Components/Container/Container.jsx
+++ app/Components/Container/Container.jsx
@@ -15,10 +15,13 @@ export default class AppContainer extends React.Component {
   render(): ?React$Element<div> {
     return(
       <section className='application-container'>
-        <div className='columns is-gapless is-mobile is-desktop'>
-          <h1>
-            Hello World
-          </h1>
+        <div className='landing-page'>
+          <div className='overlay-red bg-city'>
+            <div className='container'>
+              <h1 className='title is-fullwidth'>Nessas férias, escolha o produto certo para você.</h1>
+              <h2 className='subtitle is-half is-offset-one-quarter'>Participe da nossa Mailing List.</h2>
+            </div>
+          </div>
         </div>
       </section>
     );
