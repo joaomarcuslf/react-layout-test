7685fb46288d14b650d10701e3f21775ffee8dbd
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-12-04 18:52:13 -0200

    Added PictureCad component

---
+++ app/Components/Container/MainSection/PictureCard.jsx
@@ -0,0 +1,22 @@
+/* @flow */
+import React from 'react';
+
+export default class PictureCard extends React.Component {
+  constructor() {
+    super();
+  }
+
+  shouldComponentUpdate(): boolean {
+    return false;
+  }
+
+  render(): ?React$Element<div> {
+    let path = `assets/images/cards/${this.props.cardName}.png`;
+
+    return (
+      <div className='column is-half-mobile is-4-desktop is-4-tablet is-narrow'>
+        <img className='picture-card' src={path} />
+      </div>
+    );
+  }
+}
